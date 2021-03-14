4326d1be7a3c5401ac82eeaee6c8deafe4b74517
Huat Thart Chiang <huatc@uw.edu>
2021-03-13 16:53:10 -0800

    update tests

--- black_box_optimization/tests/test.py
+++ black_box_optimization/tests/test.py
@@ -1,1 +1,6 @@
 #testing the test 
+def test_ensemble_methods():
+	'''Runs several tests on the function'''
+	from ensemble_methods import decision_tree_classifier
+	predict = decision_tree_classifier(1,1,1,1)
+	assert type(predict) == str, 'Output is not a string'
