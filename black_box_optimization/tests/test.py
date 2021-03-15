b1143552b2362b41e7d693e0cf2a7a16bb935a2c
Huat Thart Chiang <huatc@uw.edu>
2021-03-14 18:31:22 -0700

    update

--- black_box_optimization/tests/test.py
+++ black_box_optimization/tests/test.py
@@ -1,39 +1,43 @@
-def test_ensemble_methods():
-	'''Runs several tests on the function'''
-	from black_box_optimization.ensemble_methods import decision_tree_classifier
-	from black_box_optimization.ensemble_methods import random_forest_classifier
-	import numpy as np    
-    
-	for i in range(2):
-		a = np.random.rand(1)
-		b = np.random.rand(1)
-		c = np.random.rand(1)
-		d = np.random.rand(1)
-		predict = decision_tree_classifier(a,b,c,d)
-		assert type(predict) == str, 'Output is not a string'
-		if predict == 'TPE':
-			error = 0
-		elif predict == 'CmaEs':
-			error = 0
-		elif predict == 'Random':
-			error = 0
-		elif predict == 'Bayes':
-			error = 0
-		assert error == 0, 'Output optimizer is not defined' 
+import unittest
 
-	for i in range(2):
-		a = np.random.rand(1)
-		b = np.random.rand(1)
-		c = np.random.rand(1)
-		d = np.random.rand(1)
-		predict = random_forest_classifier(a,b,c,d)
-		assert type(predict) == str, 'Output is not a string'
-		if predict == 'TPE':
-			error = 0
-		elif predict == 'CmaEs':
-			error = 0
-		elif predict == 'Random':
-			error = 0
-		elif predict == 'Bayes':
-			error = 0
-		assert error == 0, 'Output optimizer is not defined' 
+class test_Perceptron(unittest.TestCase):
+  
+    def test_ensemble_methods(self):
+        '''Runs several tests on the function'''
+        from black_box_optimization.ensemble_methods import decision_tree_classifier
+        from black_box_optimization.ensemble_methods import random_forest_classifier
+        import numpy as np    
+
+        for i in range(2):
+            a = np.random.rand(1)
+            b = np.random.rand(1)
+            c = np.random.rand(1)
+            d = np.random.rand(1)
+            predict = decision_tree_classifier(a,b,c,d)
+            assert type(predict) == str, 'Output is not a string'
+            if predict == 'TPE':
+                error = 0
+            elif predict == 'CmaEs':
+                error = 0
+            elif predict == 'Random':
+                error = 0
+            elif predict == 'Bayes':
+                error = 0
+            assert error == 0, 'Output optimizer is not defined' 
+
+        for i in range(2):
+            a = np.random.rand(1)
+            b = np.random.rand(1)
+            c = np.random.rand(1)
+            d = np.random.rand(1)
+            predict = random_forest_classifier(a,b,c,d)
+            assert type(predict) == str, 'Output is not a string'
+            if predict == 'TPE':
+                error = 0
+            elif predict == 'CmaEs':
+                error = 0
+            elif predict == 'Random':
+                error = 0
+            elif predict == 'Bayes':
+                error = 0
+            assert error == 0, 'Output optimizer is not defined' 
