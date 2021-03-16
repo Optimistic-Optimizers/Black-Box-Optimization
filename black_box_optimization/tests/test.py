8a016a3e222b363e01da9acfbca18fceefb3c087
Huat Thart Chiang <huatc@uw.edu>
2021-03-16 00:16:23 -0700

    test file

--- black_box_optimization/tests/test.py
+++ black_box_optimization/tests/test.py
@@ -1,13 +1,12 @@
 import unittest
-
 class test_Perceptron(unittest.TestCase):
-  
+    '''class test perception'''
     def test_ensemble_methods(self):
         '''Runs several tests on the function'''
         from black_box_optimization.ensemble_methods import decision_tree_classifier
         from black_box_optimization.ensemble_methods import random_forest_classifier
-        import numpy as np    
-
+        import numpy as np
+        
         for i in range(2):
             a = np.random.rand(1)
             b = np.random.rand(1)
@@ -23,8 +22,10 @@ class test_Perceptron(unittest.TestCase):
                 error = 0
             elif predict == 'Bayes':
                 error = 0
-            assert error == 0, 'Output optimizer is not defined' 
-
+            elif predict == 'PULP_CBC_CMD':
+                error = 0
+            assert error == 0, 'Output optimizer is not defined'
+            
         for i in range(2):
             a = np.random.rand(1)
             b = np.random.rand(1)
@@ -39,5 +40,7 @@ class test_Perceptron(unittest.TestCase):
             elif predict == 'Random':
                 error = 0
             elif predict == 'Bayes':
+                error = 0
+            elif predict == 'PULP_CBC_CMD':
                 error = 0
             assert error == 0, 'Output optimizer is not defined' 
